de94047065d78dfb244289490a228819902375c8
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-08-13 11:22:41 -0300

    Adding navbar.

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/lib/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/lib/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 app.use('/', index);
 app.use('/users', users);
