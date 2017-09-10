a7d1d83db6f58439aa052a88d4e048e29dbbf071
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-09-10 11:08:13 -0300

    Dynamically getting the Carousel images.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+const paintingsFolder = './public/images/paintings';
+const fs = require('fs');
+
+var paintings = [];
+
+fs.readdirSync(paintingsFolder).forEach(file => {
+  paintings.push(file);
+})
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jorge Augusto Barsoba', paintings: paintings });
 });
 
 module.exports = router;
