8fa945b7126b5f0a052a5faccabd8dcdc1c6d263
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-09-21 16:17:53 -0300

    Replacing Index carousel with Work carousel.

--- routes/work.js
+++ routes/work.js
@@ -1,17 +1,8 @@
-const paintingsFolder = './public/images/paintings';
-const fs = require('fs');
-
-var paintings = [];
-
-fs.readdirSync(paintingsFolder).forEach(file => {
-  paintings.push(file);
-})
-
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('work', { title: 'Obra - Jorge Augusto Barsoba', paintings: paintings });
+  res.render('work', { title: 'Obra - Jorge Augusto Barsoba' });
 });
 
 module.exports = router;
