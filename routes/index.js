48574f049c233011d2a63692a5edf9a731bec986
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-09-23 10:32:36 -0300

    Task, Work gallery: Set IDs for carousels, gallery-main and gallery-thumbnails; Create classes for carousels (same name than IDs), just override the heights and, in the case of gallery-main, add a margin-bottom to separate the carousels a bit.

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ fs.readdirSync(paintingsFolder).forEach(file => {
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jorge Augusto Barsoba', paintings: paintings });
 });
--- routes/work.js
+++ routes/work.js
@@ -1,8 +1,17 @@
+const paintingsFolder = './public/images/paintings';
+const fs = require('fs');
+
+var paintings = [];
+
+fs.readdirSync(paintingsFolder).forEach(file => {
+  paintings.push(file);
+})
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('work', { title: 'Obra - Jorge Augusto Barsoba' });
+  res.render('work', { title: 'Obra - Jorge Augusto Barsoba', paintings: paintings });
 });
 
 module.exports = router;
