0ff5010fdd0c2f322d056b62a72aaa3f7d5044dc
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2017-11-26 12:31:12 -0300

    Dynamically getting the number of images per thumbnails carousel item.

--- public/js/site.js
+++ public/js/site.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   $('#gallery-main').on('slid.bs.carousel', function (e) {
-    const numberOfImagesPerCarouselItem = 4;
+    const numberOfImagesPerCarouselItem = $('#gallery-thumbnails').children('.carousel-inner').children('.item').first().find('.paintings').length;
     const targetItem = $(e.relatedTarget).index();
 
     const thumbnailsItem = Math.trunc( targetItem / numberOfImagesPerCarouselItem );
